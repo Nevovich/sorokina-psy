8be31c716140518e22ae38e0f8fb5f97eecf3f3b
unknown <Nevovich@outlook.com>
2021-02-04 22:53:23 +0500

    #1. Header + post-title

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+$( document ).ready(function() {
+    // Определение ширины экрана
+    let screenWidth = window.innerWidth;
+    if (screenWidth < 992) {
+        $('#headerNavbar').before('<button id="header-navbar-toggler" class="navbar-toggler" type="button" data-toggle="collapse" data-target="#headerNavbar" aria-controls="headerNavbar" aria-expanded="false" aria-label="Toggle navigation"><img style="height: 30px;width:30px;" src="img/header/menu-icon.svg" alt="menu-icon"></button>');
+      };
+});
