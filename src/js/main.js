6022c741323f5765f06d76fc23162ec01218f991
unknown <Nevovich@outlook.com>
2021-02-05 18:14:00 +0500

    #2. post header, sidebar

--- src/js/main.js
+++ src/js/main.js
@@ -4,4 +4,5 @@ $( document ).ready(function() {
     if (screenWidth < 992) {
         $('#headerNavbar').before('<button id="header-navbar-toggler" class="navbar-toggler" type="button" data-toggle="collapse" data-target="#headerNavbar" aria-controls="headerNavbar" aria-expanded="false" aria-label="Toggle navigation"><img style="height: 30px;width:30px;" src="img/header/menu-icon.svg" alt="menu-icon"></button>');
       };
+      $('.post-date').prepend('<img class="post-date-img" src="img/post/calendar.svg" alt="calendar icon">');
 });
